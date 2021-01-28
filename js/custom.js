d3764eec72c52628a6d5c7cf8fc82a18955c5849
Zilhaz Jalal Chowdhury <zilhazjalal@gmail.com>
2021-01-28 13:28:56 -0600

    First upload

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+// $(document).on("scroll", function () {
+//   if ($(document).scrollTop() > 47) {
+//       $(".header_outer").addClass("shrink");
+//   } else {
+//       $(".header_outer").removeClass("shrink");
+//   };
+// });
